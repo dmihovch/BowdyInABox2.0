3536bf01a6cd78920ad7ab4c4902079c4ca13cc2
dmihovch <dmihovch@gmail.com>
2024-05-28 20:47:30 -0700

    added button and cleaned up junk

---
+++ src/lib/GiftBox.svelte
@@ -0,0 +1,25 @@
+<script>
+  let clicks = 0;
+  function onClick() {
+    clicks++;
+    console.log(clicks);
+    if (clicks < 5) {
+      //shake
+    }
+    if (clicks === 5) {
+      //explode
+      clicks = 0;
+    }
+  }
+</script>
+
+<button class="shake" on:click={onClick}> Hello World </button>
+
+<style>
+  .shake {
+    background-color: red;
+  }
+  .explode {
+    display: none;
+  }
+</style>
